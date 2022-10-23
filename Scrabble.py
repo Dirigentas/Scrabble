fcaca8b87a3c6250f4e78f057bae838cabd30a82
Varstasdp <d.pocius93@gmail.com>
2022-10-23 12:32:58 +0100

    Duplicate remover and pep 8 changes

--- Scrabble.py
+++ Scrabble.py
@@ -1,41 +1,50 @@
 # turimų raidzių įrašymas
 a = ''
-raides=(input("Įrašyti turimas raides (7 raidės be tarpų): ")).lower()
+temp = ""
+raides = (input("Įrašyti turimas raides (7 raidės be tarpų): ")).lower()
 print('4-7 raidžių žodžiai:')
 # importuojami visi lietuviški žodžiai
-with open("C:\\Users\\Aras\\Desktop\\Programavimas\\Python\\Scrabble\\LT zodiai.txt", encoding="UTF-8") as data_file:
+with open("C:\\Users\\dpoci\\PycharmProjects\\Scrabble-my-copy\\LT zodiai.txt", encoding="UTF-8") as data_file:
     for line in data_file:
 # /n nuėmimas
         line = line.rstrip()
 # žodžio paieška duomenų bazėje
         if sorted(line) == sorted(raides):
             a += line
-            print(line)
+            if temp != line:
+                print(line)
+            temp = line
 # nuima 1 raidę
         raides = sorted(raides)
         for c in range(7):
             removed = raides.pop(c)
             if sorted(line) == raides:
                 a += line
-                print (line)
+                if temp != line:
+                    print(line)
+                temp = line
 # nuima dar vieną raidę, tai jau 2 nuimtos
             for d in range(6):
                 removed2 = raides.pop(d)
                 if sorted(line) == raides:
                     a += line
-                    print (line)
+                    if temp != line:
+                        print(line)
+                    temp = line
 # nuima dar vieną raidę, tai jau 3 nuimtos
                 for e in range(5):
                     removed3 = raides.pop(e)
                     if sorted(line) == raides:
                         a += line
-                        print (line)
+                        if temp != line:
+                            print(line)
+                        temp = line
                     raides.append(removed3)
                     raides = sorted(raides)
                 raides.append(removed2)
                 raides = sorted(raides)
             raides.append(removed)
-            raides=sorted(raides)
+            raides = sorted(raides)
 print('Su išvardintomis raidėmis žodžių nerasta' if not a.isalpha() else '!!!')
 
 
